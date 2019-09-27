2a4672102926c5a8d6b7f3d19ea01d74944980dc
Michal Jewczuk <michal.jewczuk@outlook.com>
2019-09-27 14:28:20 +0200

    Implemented httpService methods to call mock api

--- src/main/java/com/example/batchforscience/processor/ClientItemProcessor.java
+++ src/main/java/com/example/batchforscience/processor/ClientItemProcessor.java
@@ -1,10 +1,15 @@
 package com.example.batchforscience.processor;
 
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.example.batchforscience.domain.Client;
+import com.example.batchforscience.service.ClientService;
 
 public class ClientItemProcessor implements ItemProcessor<Client, Client> {
+	
+	@Autowired
+	private ClientService clientService;
 
 	@Override
 	public Client process(Client item) throws Exception {
--- src/main/java/com/example/batchforscience/service/impl/HttpServiceImpl.java
+++ src/main/java/com/example/batchforscience/service/impl/HttpServiceImpl.java
@@ -1,8 +1,13 @@
 package com.example.batchforscience.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import com.example.batchforscience.domain.Invoice;
 import com.example.batchforscience.domain.Order;
@@ -11,17 +16,34 @@ import com.example.batchforscience.service.HttpService;
 @Component
 public class HttpServiceImpl implements HttpService {
 
-	private static final String INVOICE_URI = "http://localhost:8080/invoices/";
-	private static final String ORDER_URI = "http://localhost:8080/orders/";
+	private static final String INVOICE_URI = "http://localhost:8080/mock/invoices/";
+	private static final String ORDER_URI = "http://localhost:8080/mock/orders/";
+	private RestTemplate restTemplate = new RestTemplate();
 	
 	public List<Invoice> getClientInvoices(Long clientId) {
-		// TODO Auto-generated method stub
-		return null;
+		ParameterizedTypeReference<List<Invoice>> typeRef = new ParameterizedTypeReference<List<Invoice>>() {};
+		List<Invoice> invoices = new ArrayList<Invoice>();
+		try {
+			ResponseEntity<List<Invoice>> response = restTemplate.exchange(INVOICE_URI + clientId, HttpMethod.GET, null, typeRef);
+			invoices = response.getBody();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		
+		return invoices;
 	}
 	
 	public List<Order> getClientOrders(Long clientId) {
-		// TODO Auto-generated method stub
-		return null;
+		ParameterizedTypeReference<List<Order>> typeRef = new ParameterizedTypeReference<List<Order>>() {};
+		List<Order> orders = new ArrayList<Order>();
+		try {
+			ResponseEntity<List<Order>> response = restTemplate.exchange(ORDER_URI + clientId, HttpMethod.GET, null, typeRef);
+			orders = response.getBody();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		
+		return orders;
 	}
 	
 }
